4bb79f39a87a4255c1d761c76945c0c0e1e647b3
mjstrz <strzelecki.monica7@gmail.com>
2022-03-02 20:39:21 -0500

    Mongoose ODM part 3

---
+++ routes/promotionRouter.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const promotionRouter = express.Router();
+
+
+promotionRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the promotions to you');
+})
+.post((req, res) => {
+    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res) => {
+    res.end('Deleting all promotions');
+});
+
+promotionRouter.route('/:promotionsId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+    .get((req, res) => {
+        res.end(`Will send all the ${req.params.promotionsId} to you`);
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /promotions/id');
+    })
+    .put((req, res) => {
+        res.end(`Will update ${req.params.promotionsId} with ${req.body.name}`);
+    })
+    .delete((req, res) => {
+        res.end(`Deleting promotion: ${req.params.promotionsId}`);
+    });
+
+module.exports = promotionRouter;
